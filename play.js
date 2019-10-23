f7dab92ac7665934b61e186c2844801b770e2c45
Praneeth <praneethabc123@gmail.com>
2019-10-23 21:50:19 +0000

    Updated to add new functions

--- play.js
+++ play.js
@@ -1,3 +1,16 @@
 const connect = require('./client');
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+ const handleUserInput = function(data){
+    if (data === "\u0003"){
+      process.exit();
+    }
+  }
+  stdin.on(handleUserInput(data));
+  return stdin;
+}
 console.log('Connecting ...');
 connect();
